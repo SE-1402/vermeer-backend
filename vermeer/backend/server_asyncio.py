84f856e3b033b3b27eec61334318f65ed1fda29d
John Shelley <john.shelley.p@gmail.com>
2014-11-05 22:36:02 -0600

    (node) Restore iop parser

--- vermeer/backend/server_asyncio.py
+++ vermeer/backend/server_asyncio.py
@@ -18,7 +18,7 @@
 import json
 
 from autobahn.asyncio.websocket import WebSocketServerProtocol, WebSocketServerFactory
-#from vermeer.backend.util.isobus_converter import IopParser
+from vermeer.backend.util.isobus_converter import IopParser
 
 
 class MyServerProtocol(WebSocketServerProtocol):
@@ -32,28 +32,28 @@ class MyServerProtocol(WebSocketServerProtocol):
         if not isBinary:
             print("Text message received: {0}".format(payload.decode('utf8')))
             message = payload.decode('utf8')
-            # if 'connect' in message:
-            # # First time Client it connecting: Send decoded .iop file.
-            #     msg = "{} from {}".format(message, self.peer)
-            #     iop_parser = IopParser()
-            #     try:
-            #         objects = iop_parser.parse("./vermeer/backend/util/example.iop")
-            #         # TODO: Broadcast the objects in json format
-            #         self.sendMessage(payload, isBinary)
-            #         self.sendMessage(payload, isBinary)
-            #     except Exception, e:
-            #         print e
-            # elif 'test' in message:
-            #     try:
-            #         json_data = open('./vermeer/backend/util/iop.json')
-            #         data = json.load(json_data)
-            #         payload = json.dumps(data, ensure_ascii=False).encode('utf8')
-            #         ## echo back message verbatim
-            #         self.sendMessage(payload, isBinary)
-            #     except Exception, e:
-            #         print e
-            # else:
-            self.sendMessage(payload, isBinary)
+            if 'connect' in message:
+            # First time Client it connecting: Send decoded .iop file.
+                msg = "{} from {}".format(message, self.peer)
+                iop_parser = IopParser()
+                try:
+                    objects = iop_parser.parse("./vermeer/backend/util/example.iop")
+                    # TODO: Broadcast the objects in json format
+                    self.sendMessage(payload, isBinary)
+                    self.sendMessage(payload, isBinary)
+                except Exception, e:
+                    print e
+            elif 'test' in message:
+                try:
+                    json_data = open('./vermeer/backend/util/iop.json')
+                    data = json.load(json_data)
+                    payload = json.dumps(data, ensure_ascii=False).encode('utf8')
+                    ## echo back message verbatim
+                    self.sendMessage(payload, isBinary)
+                except Exception, e:
+                    print e
+            else:
+                self.sendMessage(payload, isBinary)
         else:
             print("Binary message received: {0} bytes".format(len(payload)))
 
